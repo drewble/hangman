bd9e9f1c2cfcd2978c1ab4815bfe75bffa242ace
Drew Blake <drew@dmblake.com>
2021-05-16 13:52:59 -0500

    Finished building game backend

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,4 +1,19 @@
 defmodule Hangman do
   alias Hangman.Game
   defdelegate new_game(), to: Game
+  defdelegate tally(game), to: Game
+  # defdelegate make_move(game, guess), to: Game
+
+  def make_move(game, guess) do
+    do_make_move(game, guess, guess =~ ~r/^[a-z]$/)
+  end
+
+  def do_make_move(_game, _guess, false) do
+    raise "ERROR: Guess must be a single, lowercase ASCII character."
+  end
+
+  def do_make_move(game, guess, true) do
+    game = Game.make_move(game, guess)
+    {game, tally(game)}
+  end
 end
